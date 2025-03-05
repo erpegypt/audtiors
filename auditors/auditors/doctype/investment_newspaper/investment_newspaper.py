6172d46c5b8245631183335e257d5fa8882b6055
Connect 4 Systems <info@connect4systems.com>
2025-03-05 15:45:24 +0200

    commit

---
+++ auditors/auditors/doctype/investment_newspaper/investment_newspaper.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2024, Connect 4 Systems and contributors
+# For license information, please see license.txt
+
+# import frappe
+from frappe.model.document import Document
+
+
+class Investmentnewspaper(Document):
+	pass
